f852997d02086af343edb8c8787be09e6cf3e29e
Zijun WU <zijunwu@ZijundeMacBook-Air.local>
2022-12-09 11:31:05 +0800

    update

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,5 +1,23 @@
 -- Additional Plugins
 lvim.plugins = {
+  { 'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async' },
+  {
+    "kevinhwang91/nvim-hlslens",
+    config = function()
+      require("hlslens").setup({
+        build_position_cb = function(plist, _, _, _)
+          require("scrollbar.handlers.search").handler.show(plist.start_pos)
+        end,
+      })
+
+      vim.cmd([[
+        augroup scrollbar_search_hide
+            autocmd!
+            autocmd CmdlineLeave : lua require('scrollbar.handlers.search').handler.hide()
+        augroup END
+    ]] )
+    end,
+  },
   {
     'keaising/im-select.nvim',
     config = function()
