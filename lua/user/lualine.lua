f15b19b419df32fe02fcbd1399a4a72ad4fb3317
Zijun WU <zijunwu@ZijundeMacBook-Air.local>
2022-12-23 15:45:14 +0800

    update

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,8 +1,8 @@
 lvim.builtin.lualine.sections.lualine_z = { 'os.date("%I:%M", os.time())' }
 local components = require "lvim.core.lualine.components"
 lvim.builtin.lualine.sections.lualine_c = { components.diff, components.python_env, components.filename }
-lvim.builtin.lualine.options.section_separators = { left = '', right = '' }
-lvim.builtin.lualine.options.component_separators = { left = '', right = '' }
+-- lvim.builtin.lualine.options.section_separators = { left = '', right = '' }
+-- lvim.builtin.lualine.options.component_separators = { left = '', right = '' }
 -- separators = {
 --   rounded = { '', '' },
 --   blank = { '', '' },
