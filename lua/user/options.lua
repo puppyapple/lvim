9cf8634adb75ed1dc079cf71257b3714ea092589
Zijun WU <zijunwu@ZijundeMacBook-Air.local>
2022-12-23 16:10:19 +0800

    update

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -8,7 +8,7 @@ lvim.builtin.cmp.cmdline.enable = true
 lvim.builtin.bufferline.options.separator_style = "slant"
 lvim.builtin.bufferline.options.always_show_bufferline = true
 
-vim.highlight.on_yank { timeout = 2000 }
+vim.highlight.on_yank {timeout = 2000}
 vim.opt.relativenumber = true
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
@@ -26,23 +26,23 @@ lvim.builtin.nvimtree.setup.renderer.highlight_git = true
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
 lvim.builtin.nvimtree.setup.renderer.icons.git_placement = "before"
 lvim.builtin.nvimtree.setup.renderer.icons.glyphs.git = {
-  unstaged = "✗",
-  staged = "✓",
-  unmerged = "",
-  renamed = "➜",
-  untracked = "★",
-  deleted = "",
-  ignored = "◌"
+    unstaged = "✗",
+    staged = "✓",
+    unmerged = "",
+    renamed = "➜",
+    untracked = "★",
+    deleted = "",
+    ignored = "◌"
 }
 lvim.builtin.nvimtree.setup.renderer.indent_markers.enable = true
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
-  "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx",
-  "css", "rust", "java", "yaml"
+    "bash", "c", "javascript", "json", "lua", "python", "typescript", "tsx",
+    "css", "rust", "java", "yaml"
 }
 
-lvim.builtin.treesitter.ignore_install = { "haskell" }
+lvim.builtin.treesitter.ignore_install = {"haskell"}
 -- lvim.builtin.treesitter.highlight.enable = true
-local options = { scrolloff = 0, sidescrolloff = 8, updatetime = 100 }
+local options = {scrolloff = 0, sidescrolloff = 8, updatetime = 100}
 for k, v in pairs(options) do vim.opt[k] = v end
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -138,68 +138,6 @@ lvim.plugins = {
   {
     "navarasu/onedark.nvim",
     as = "onedark",
-    -- config = function()
-    --   -- require('onedark').load()
-    --   require('onedark').setup {
-    --     -- Main options --
-    --     style = 'deep', -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-    --     transparent = false, -- Show/hide background
-    --     term_colors = true, -- Change terminal color as per the selected theme style
-    --     ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-    --     cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
-
-    --     -- toggle theme style ---
-    --     toggle_style_key = nil, -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-    --     toggle_style_list = {
-    --       'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
-    --     }, -- List of styles to toggle between
-
-    --     -- Change code style ---
-    --     -- Options are italic, bold, underline, none
-    --     -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
-    --     code_style = {
-    --       -- comments = 'italic',
-    --       keywords = 'none',
-    --       functions = 'none',
-    --       strings = 'none',
-    --       variables = 'none'
-    --     },
-    --     -- Lualine options --
-
-    --     lualine = {
-    --       transparent = false -- lualine center bar transparency
-    --     },
-
-    --     -- Custom Highlights --
-    --     -- MiniCursorword = { fmt = "underline" }, MiniCursorwordCurrent = { fmt = "underline" },
-    --     colors = {}, -- Override default colors
-    --     highlights = {
-    --       ["@function"] = { fg = "#2c56ff" },
-    --       ["@parameter"] = { fmt = "italic" },
-    --       ["@field"] = { fg = "#10deb5" },
-    --       ["@variable"] = { fg = "#7b64ff" },
-    --       ["@type"] = { fg = "#e00f9a" },
-    --       ["@comment"] = { fg = "#278505" },
-    --       ["@keyword"] = { fmt = "italic" },
-    --       ["@keyword.operator"] = { fmt = "italic" },
-    --       ["@function.builtin"] = { fmt = "italic" },
-    --       ["@repeat"] = { fmt = "italic" },
-    --       ["@conditional"] = { fmt = "italic" },
-    --       ["@include"] = { fmt = "italic" },
-    --       ["@variable.builtin"] = { fg = "#e5c07b" },
-    --       ["@property"] = { fg = "red", fmt = "italic" },
-    --       ["@local.definition"] = { fg = "red", fmt = "italic" }
-    --     }, -- Override highlight groups
-
-    --     -- Plugins Config --
-    --     darker = true, -- darker colors for diagnostic
-    --     diagnostics = {
-    --       undercurl = true, -- use undercurl instead of underline for diagnostics
-    --       background = true -- use background color for virtual text
-    --     }
-    --   }
-    --   -- vim.api.nvim_command "colorscheme onedark"
-    -- end
   },
   {
     "EdenEast/nightfox.nvim",
