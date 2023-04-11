02d83efb369d9282cf6f2b3d333059a014b89bde
wuzijun <wuzijun@bigai.ai>
2023-04-11 09:38:57 +0800

    update

--- after/ftplugin/python.lua
+++ after/ftplugin/python.lua
@@ -1,35 +1,36 @@
 local opts = {
-    root_dir = function(fname)
-        local util = require "lspconfig.util"
-        local root_files = {
-            "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt",
-            "Pipfile", "manage.py", "pyrightconfig.json"
-        }
-        return util.root_pattern(unpack(root_files))(fname) or
-                   util.root_pattern ".git"(fname) or util.path.dirname(fname)
-    end,
-    settings = {
-        pyright = {
-            disableLanguageServices = false,
-            disableOrganizeImports = false
-        },
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "workspace",
-                useLibraryCodeForTypes = true
-            }
-        }
+  root_dir = function(fname)
+    local util = require "lspconfig.util"
+    local root_files = {
+      "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt",
+      "Pipfile", "manage.py", "pyrightconfig.json"
+    }
+    return util.root_pattern(unpack(root_files))(fname) or
+        util.root_pattern ".git" (fname) or util.path.dirname(fname)
+  end,
+  settings = {
+    pyright = {
+      disableLanguageServices = false,
+      disableOrganizeImports = false
     },
-    single_file_support = true
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = "workspace",
+        useLibraryCodeForTypes = true
+      }
+    }
+  },
+  single_file_support = true
 }
 
 require("lvim.lsp.manager").setup("pyright", opts)
 local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {{command = "black", filetypes = {"python"}}}
+formatters.setup { { command = "black", filetypes = { "python" } } }
 
 local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {{command = "ruff", filetypes = {"python"}}}
+linters.setup { { command = "ruff", filetypes = { "python" } },
+  { command = "codespell", filetypes = { "javascript", "python" } } }
 
 local code_actions = require "lvim.lsp.null-ls.code_actions"
-code_actions.setup {{command = "proselint"}}
+code_actions.setup { { command = "proselint" } }
