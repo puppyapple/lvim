a1858b4cb1d5fbec7e8dcf94b80bc8f418c02d6d
wuzijun <wuzijun@bigai.ai>
2023-05-12 11:19:43 +0800

    update

--- lua/user/themes.lua
+++ lua/user/themes.lua
@@ -1,55 +1,5 @@
--- Palettes are the base color defines of a colorscheme.
--- You can override these palettes for each colorscheme defined by nightfox.
-local palettes = {}
-
--- Spec's (specifications) are a mapping of palettes to logical groups that will be
--- used by the groups. Some examples of the groups that specs map would be:
---   - syntax groups (functions, types, keywords, ...)
---   - diagnostic groups (error, warning, info, hints)
---   - git groups (add, removed, changed)
---
--- You can override these just like palettes
-local specs = {}
-
--- Groups are the highlight group definitions. The keys of this table are the name of the highlight
--- groups that will be overridden. The value is a table with the following values:
---   - fg, bg, style, sp, link,
---
--- Just like `spec` groups support templates. This time the template is based on a spec object.
-local groups = {
-  -- As with specs and palettes, the values defined under `all` will be applied to every style.
-  -- all = {
-  --   -- If `link` is defined it will be applied over any other values defined
-  --   Whitespace = { link = "Comment" },
-
-  --   -- Specs are used for the template. Specs have their palette's as a field that can be accessed
-  --   IncSearch = { bg = "palette.cyan" },
-  -- },
-  -- nightfox = {
-  --   -- As with specs and palettes, a specific style's value will be used over the `all`'s value.
-  --   PmenuSel = { bg = "#73daca", fg = "bg0" },
-  all = {
-    ["@function"] = { fg = "#2c56ff" },
-    parameter = { fmt = "italic" },
-    field = { fg = "#10deb5" },
-    variable = { fg = "#7b64ff" },
-    type = { fg = "#e00f9a" },
-    comment = { fg = "#278505" },
-    keyword = { fmt = "italic" },
-    ["@keyword.operator"] = { fmt = "italic" },
-    ["@function.builtin"] = { fmt = "italic" },
-    ["@repeat"] = { fmt = "italic" },
-    conditional = { fmt = "italic" },
-    include = { fmt = "italic" },
-    ["@variable.builtin"] = { fg = "#e5c07b" },
-    property = { fg = "red", fmt = "italic" },
-    ["@local.definition"] = { fg = "red", fmt = "italic" }
-  } -- Override highlight groups
-  -- },
-}
-
-require("nightfox").setup({ palettes = palettes, specs = specs, groups = groups })
-
+-- Ondark theme
+----------------------------------------------------------------------------------------------------------------
 local my_highlight = {
   ["@function"] = { fg = "#2c56ff" },
   ["@parameter"] = { fmt = "italic" },
@@ -83,8 +33,7 @@ require('onedark').setup {
     'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'
   }, -- List of styles to toggle between
 
-  -- Change code style ---
-  -- Options are italic, bold, underline, none
+  -- Change code style --- Options are italic, bold, underline, none
   -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
   code_style = {
     -- comments = 'italic',
@@ -113,9 +62,30 @@ require('onedark').setup {
     background = true -- use background color for virtual text
   }
 }
+----------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+-- Tokyonight theme
+----------------------------------------------------------------------------------------------------------------
 local util = require("tokyonight.util")
 require("tokyonight").setup {
   style = "night",
+  styles = {
+    -- Style to be applied to different syntax groups
+    -- Value is any valid attr-list value for `:help nvim_set_hl`
+    comments = { italic = true },
+    keywords = { italic = true },
+    functions = {},
+    variables = {},
+    -- Background styles. Can be "dark", "transparent" or "normal"
+    sidebars = "transparent", -- style for sidebars, see below
+    floats = "dark",          -- style for floating windows
+  },
   on_highlights = function(hl, c)
     hl.Hlargs = { fg = "#D49DA5" }
     hl.DiagnosticVirtualTextHint = { bg = util.darken(c.magenta, 0.1), fg = c.magenta }
@@ -143,5 +113,4 @@ require("tokyonight").setup {
   end
 }
 
--- setup must be called before loading
--- vim.api.nvim_command "colorscheme carbonfox"
+----------------------------------------------------------------------------------------------------------------
--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -1,13 +1,13 @@
 local Terminal = require('toggleterm.terminal').Terminal
 local lazygit = Terminal:new({
-    cmd = "lazygit",
-    hidden = true,
-    direction = "float",
-    float_opts = {border = "curved"}
+  cmd = "lazygit",
+  hidden = true,
+  direction = "float",
+  float_opts = { border = "curved" }
 })
 
 function lazygit_toggle() lazygit:toggle() end
 
 lvim.builtin.which_key.mappings["gg"] = {
-    "<cmd>lua lazygit_toggle()<cr>", "Lazygit"
+  "<cmd>lua lazygit_toggle()<cr>", "Lazygit"
 }
